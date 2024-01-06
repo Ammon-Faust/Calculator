33edb8e6609a3a555a55388a2ea1286b836d0fbd
Ammon Faust <119088185+Ammon-Faust@users.noreply.github.com>
2024-01-05 21:27:24 -0700

    add: js button function to update display, styled the text

---
+++ script.js
@@ -0,0 +1,17 @@
+const display = document.getElementById('display');
+
+function updateDisplay(input) {
+    display.value += input;
+};
+
+function clearDisplay() {
+
+};
+
+function backSpace() {
+
+};
+
+function calculate(){
+
+}
